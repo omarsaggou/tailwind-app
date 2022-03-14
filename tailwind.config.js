9de8dfceb3b87aeeb9da95d3c67ed3396753b112
SAGGOU <39929719+omarsaggou@users.noreply.github.com>
2022-03-14 17:43:09 +0100

    contact part

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,39 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    flex: {
+      '1/2': '1 1 50%',
+      '1/4': '1 1 25%',
+    },
+    screens: {
+      'xs': '480px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px'
+    },
+    extend: {
+      animation: {
+        blob: "blob 10s infinite",
+      },
+      keyframes: {
+        blob: {
+          "0%": {
+            transform: "translate(0px, 0px) scale(0.5)",
+          },
+          "33%": {
+            transform: "translate(10px, -20px) scale(0.7)",
+          },
+          "66%": {
+            transform: "translate(-20px, 20px) scale(0.9)",
+          },
+          "100%": {
+            transform: "tranlate(0px, 0px) scale(1)",
+          },
+        },
+      },
+    },
   },
-  plugins: [require('@tailwindcss/aspect-ratio'), require('@tailwindcss/typography')],
+  plugins: [require('@tailwindcss/aspect-ratio'), require('@tailwindcss/typography'), require('@tailwindcss/forms')],
 }
