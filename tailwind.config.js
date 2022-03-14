3b2c9c786552429f1a21ea0604c08b09aefd341d
Noureddinebouamrane <noureddinebouamrane@gmail.com>
2022-03-14 16:19:06 +0100

    footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,30 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        blob: "blob 7s infinite",
+      },
+      keyframes: {
+        blob: {
+          "0%": {
+            transform: "translate(0px, 0px) scale(1)",
+          },
+          "33%": {
+            transform: "translate(30px, -50px) scale(1.3)",
+          },
+          "50%": {
+            transform: "translate(30px, -50px) scale(1)",
+          },
+          "66%": {
+            transform: "translate(-20px, 20px) scale(0.7)",
+          },
+          "100%": {
+            transform: "tranlate(0px, 0px) scale(1)",
+          },
+        },
+      },
+    },
   },
-  plugins: [require('@tailwindcss/aspect-ratio')],
-}
+  plugins: [require("@tailwindcss/aspect-ratio")],
+};
