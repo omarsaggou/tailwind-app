6aa4f51d259b2af676d681c5b39353618c99b811
SAGGOU <39929719+omarsaggou@users.noreply.github.com>
2022-03-16 19:21:40 +0100

    CAT

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      flex: {
+        '1/2': '1 1 50%',
+        '1/4': '1 1 25%',
+      },
+      screens: {
+        'xs': '480px',
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px'
+      }
+    },
   },
   plugins: [require('@tailwindcss/aspect-ratio')],
 }
